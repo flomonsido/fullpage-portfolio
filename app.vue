4c095e752897ce095747f73d5c163794c80d8ce1
Filipe Lourenco <93579791+flomonsido@users.noreply.github.com>
2024-07-05 13:58:24 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -2,5 +2,7 @@
   <div>
     <NuxtWelcome />
     <h1> Test </h1>
+    <a href="https://monsidotest.neocities.org/" class="btn" onclick="window.monsido_functions.trackEvent('LoginPage', 'click', 'button - new website', 1);">Check this out</a>
+
   </div>
 </template>
